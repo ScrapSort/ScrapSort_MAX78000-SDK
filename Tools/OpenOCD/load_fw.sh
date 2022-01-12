53f299b2802d02be1e09cba4acf533be29543e45
Geffen Cooper <geffenc2789@gmail.com>
2022-01-11 22:45:45 -0800

    new CNN iteration works well

--- Tools/OpenOCD/load_fw.sh
+++ Tools/OpenOCD/load_fw.sh
@@ -6,4 +6,4 @@
 #sudo ./openocd -f interface/cmsis-dap.cfg -f target/max78000.cfg -s ./scripts -c "program ../../Examples/MAX78000/CNN/sorting_running/build/max78000.elf verify reset exit"
 
 #sudo ./openocd -f interface/cmsis-dap.cfg -f target/max78000.cfg -s ./scripts -c "program ../../Examples/MAX78000/CNN/sort_aug/build/max78000.elf verify reset exit"
-sudo ./openocd -f interface/cmsis-dap.cfg -f target/max78000.cfg -s ./scripts -c "program ../../Examples/MAX78000/CNN/simplesort3/build/max78000.elf verify reset exit"
+sudo ./openocd -f interface/cmsis-dap.cfg -f target/max78000.cfg -s ./scripts -c "program ../../Examples/MAX78000/CNN/simplesortbn/build/max78000.elf verify reset exit"
