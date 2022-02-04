457e58f9f003d75727a5c8ab5b285d2bce2e8ce5
Bethany Long <bethanylong@ucsb.edu>
2022-02-03 16:29:58 -0800

    ir sensor triggers motor rotation :)

---
+++ Examples/MAX78000/Stepper_Control/ir_gpio_funcs.c
@@ -0,0 +1,34 @@
+/***** Includes *****/
+
+// GPIO
+#include <stdio.h>
+#include <string.h>
+#include "mxc_device.h"
+#include "mxc_delay.h"
+#include "nvic_table.h"
+#include "pb.h"
+#include "board.h"
+#include "gpio.h"
+
+#include "tmr_funcs.h"
+#include "ir_gpio_funcs.h"
+#include "motor_funcs.h"
+
+/***** Functions *****/
+
+void gpio_isr(void* cbdata)
+{
+    mxc_gpio_cfg_t* cfg = cbdata;
+    MXC_GPIO_OutToggle(cfg->port, cfg->mask);
+    
+    if (pause_ir_interrupts) return;
+
+    printf("interrupt\n");
+
+    rotate_revs(0, 2);
+
+    NVIC_SetVector(TMR5_IRQn, OneshotTimerHandler);
+    NVIC_EnableIRQ(TMR5_IRQn);
+    
+    OneshotTimer();
+}
