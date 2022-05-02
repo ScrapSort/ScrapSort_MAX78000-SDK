03d19423247282bec794453ba98e6803b5766a0c
Geffen Cooper <geffenc2789@gmail.com>
2022-05-01 19:35:54 -0700

    fixed build issues

--- Application/main.c
+++ Application/main.c
@@ -1,49 +1,3 @@
-/**
- * @file        main.c
- * @brief       I2C Loopback Example
- * @details     This example uses the I2C Master to read/write from/to the I2C Slave. For
- *              this example you must connect P0.12 to P0.18 (SCL) and P0.13 to P0.19 (SCL). The Master
- *              will use P0.12 and P0.13. The Slave will use P0.18 and P0.19. You must also
- *              connect the pull-up jumpers (JP23 and JP24) to the proper I/O voltage.
- *              Refer to JP27 to determine the I/O voltage.
- * @note        Other devices on the EvKit will be using the same bus. This example cannot be combined with
- *              a PMIC or bluetooth example because the I2C Slave uses GPIO pins for those devices.
- */
-
-/*******************************************************************************
-* Copyright (C) Maxim Integrated Products, Inc., All Rights Reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a
-* copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,
-* and/or sell copies of the Software, and to permit persons to whom the
-* Software is furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included
-* in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-* IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
-* OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-* OTHER DEALINGS IN THE SOFTWARE.
-*
-* Except as contained in this notice, the name of Maxim Integrated
-* Products, Inc. shall not be used except as stated in the Maxim Integrated
-* Products, Inc. Branding Policy.
-*
-* The mere transfer of this software does not imply any licenses
-* of trade secrets, proprietary technology, copyrights, patents,
-* trademarks, maskwork rights, or any other form of intellectual
-* property whatsoever. Maxim Integrated Products, Inc. retains all
-* ownership rights.
-*
-******************************************************************************/
-
-
 /***** Includes *****/
 #include <stdio.h>
 #include <stdint.h>
@@ -63,121 +17,77 @@
 #include "I2C_funcs.h"
 #include "motor_funcs.h"
 #include "tmr_funcs.h"
-#include "ir_gpio_funcs.h"
-
-#include "sorter.h"
 #include "cnn_helper_funcs.h"
 #include "camera_tft_funcs.h"
-#include "ultrasonic.h"
 
-//#define COLLECT_DATA
-//#define STREAM_MODE
-
-#ifdef COLLECT_DATA
-#include "capture_button.h"
-#endif
 
 
 // *****************************************************************************
 int main()
 {
-    MXC_ICC_Enable(MXC_ICC0); // Enable cache
-
     // Switch to 100 MHz clock
     MXC_SYS_Clock_Select(MXC_SYS_CLOCK_IPO);
     SystemCoreClockUpdate();
-    
-    // set up the camera and LCD
-    //LCD_Camera_Setup();
 
-    #ifdef COLLECT_DATA
-    init_card();
-    init_class_button();
-    init_capture_button();
-    #endif
-
-    #ifndef STREAM_MODE
-    // SYSTICK
+    // initialize systick
     SysTick_Setup();
-    #endif
+
+    LCD_Camera_Setup();
+
+    // init I2C
+    if (I2C_Init() != E_NO_ERROR) 
+    {
+        printf("I2C INITIALIZATION FAILURE\n");
+    } 
+    else 
+    {
+        printf("I2C INITIALIZED :)\n");
+    } 
     
-    #ifndef COLLECT_DATA
-    // init the CNN accelerator
-    //startup_cnn();
+    // Initialize test data
+    for (int i = 0; i < I2C_BYTES; i++) 
+    {
+        txdata[i] = 0;
+        rxdata[i] = 0;
+    }
 
-    #ifndef STREAM_MODE
-    // init the IR GPIOs
-    //gpio_init();
-    #endif
-
-    // init the PWM & TMR
-    //PWMTimer();
+    // init MOTORS
+    if (Motor_Init_Settings() != E_NO_ERROR) 
+    {
+        printf("MOTOR SETTINGS INITIALIZATION FAILURE\n");
+    } 
+    else 
+    {
+        printf("MOTOR SETTINGS INITIALIZED :)\n");
+    }
     
-    #ifndef STREAM_MODE
-    // // init I2C
-    // if (I2C_Init() != E_NO_ERROR) 
-    // {
-    //     printf("I2C INITIALIZATION FAILURE\n");
-    // } 
-    // else 
-    // {
-    //     printf("I2C INITIALIZED :)\n");
-    // } 
-    
-    // // Initialize test data
-    // for (int i = 0; i < I2C_BYTES; i++) 
-    // {
-    //     txdata[i] = 0;
-    //     rxdata[i] = 0;
-    // }
+    // initialize the ultrasonic sensor pins
+    init_trigger_gpios();  
+    init_echo_gpios();
 
-    // // init MOTORS
-    // if (Motor_Init_Settings() != E_NO_ERROR) 
-    // {
-    //     printf("MOTOR SETTINGS INITIALIZATION FAILURE\n");
-    // } 
-    // else 
-    // {
-    //     printf("MOTOR SETTINGS INITIALIZED :)\n");
-    // }
-    #endif
-    #endif
+    // set the motor profile for this test
+    set_motor_profile(0, MOTOR_PROFILE_SPEED);
+    set_motor_profile(1, MOTOR_PROFILE_SPEED);
+    set_motor_profile(2, MOTOR_PROFILE_SPEED);
 
-    // cnn_output_t output;
-    // set_motor_profile(0, MOTOR_PROFILE_SPEED);
-    // set_motor_profile(1, MOTOR_PROFILE_SPEED);
-    // set_motor_profile(2, MOTOR_PROFILE_SPEED);
+    startup_cnn();
 
-    init_trigger();  
-    init_ultrasonic_gpios();
+    // activate the first ultrasonic sensor
+    activate_triggercam();
 
     // ======================== Main Loop =========================
+    
     while(1) 
     {
-        #ifndef COLLECT_DATA
-        #ifndef STREAM_MODE
-        // keep checking for interrupt flags
+        // check interrupt callbacks (code that should be executed outside interrupts)
         check_all_callbacks();
-        #endif
-        #endif
-        
-        #ifdef COLLECT_DATA
-        capture_camera_img();
-      
-        display_RGB565_img(56,96,NULL,false);
-        if(clicked() == 1)
-        {
-            capture();
-        }
-        if(switched() == 1)
-        {
-            switch_class();
-        }
-        #endif
 
-        #ifdef STREAM_MODE
-        output = *run_cnn();
-        show_cnn_output(output);
-        #endif
+        // check if the next ultrasonic sensor should be triggered
+        to_trigger();
+
+        if(global_counter % 20000 == 0)
+        {
+            get_heartbeat();
+        }
     }
 }
