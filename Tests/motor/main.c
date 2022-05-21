d799b26e1888c941fa1f761c99374c43d2f9fa09
taco650 <taco650@gmail.com>
2022-05-21 15:53:58 -0700

    stable

--- Tests/motor/main.c
+++ Tests/motor/main.c
@@ -1,6 +1,6 @@
 #define FIRST_MOTOR_TEST_NUM 1
 #define LAST_MOTOR_TEST_NUM 1
-#define MOTOR_TEST_NUM 1
+#define MOTOR_TEST_NUM 0
 #define IN_TIC_NUM 10
 #define OUT_TIC_NUM -110
 
@@ -71,37 +71,60 @@ int main()
     
     set_current_limit(motors[0], 13);
     set_current_limit(motors[1], 20);
-    set_current_limit(motors[2], 30);
+    set_current_limit(motors[2], 13);
 
     set_motor_profile(motors[0], MOTOR_PROFILE_DEFAULT);
     set_motor_profile(motors[1], MOTOR_PROFILE_DEFAULT);
     set_motor_profile(motors[2], MOTOR_PROFILE_DEFAULT);
-
-    
-    printf("Steps: %f\n", get_max_step(motors[MOTOR_TEST_NUM]));
-    printf("Max microstep: %d\n", motors[MOTOR_TEST_NUM]->maxMicrostep);
+    energize(motors[0]);
+    energize(motors[1]);
+    energize(motors[2]);
+    calibrate_motors(motors, 3);
+    printf("Motor 1: %d\n", motors[0]->maxMicrostep);
+    printf("Motor 2: %d\n", motors[1]->maxMicrostep);
+    printf("Motor 3: %d\n", motors[2]->maxMicrostep);
+    printf("Motor 1: %d\n", motors[0]->maxStep);
+    printf("Motor 2: %d\n", motors[1]->maxStep);
+    printf("Motor 3: %d\n", motors[2]->maxStep);
+    // energize(motors[MOTOR_TEST_NUM]);
+    // MXC_Delay(MSEC(250));   
+    // printf("Steps: %f\n", get_max_step(motors[MOTOR_TEST_NUM]));
+    // printf("Max microstep: %d\n", motors[MOTOR_TEST_NUM]->maxMicrostep);
     // printf("Curr Angle: %f\n", get_angle(motors[0]));
     // set_motor_profile(motors[0], MOTOR_PROFILE_TORQUE);
     // set_angle(motors[0], 90);
     // printf("Current Postion: %d\n", get_current_position(motors[0]));
     // wait_for_target(motors[0]);
-    energize(motors[MOTOR_TEST_NUM]);
-    block_object(motors[MOTOR_TEST_NUM]);
-    MXC_Delay(SEC(2));
-    pull_object(motors[MOTOR_TEST_NUM]);
-    MXC_Delay(SEC(2));
-    go_home_forward(motors[MOTOR_TEST_NUM]);
-    wait_for_home(motors[MOTOR_TEST_NUM]);
-    MXC_Delay(SEC(2));
-    block_object(motors[MOTOR_TEST_NUM]);
-    MXC_Delay(SEC(2));
-    push_object(motors[MOTOR_TEST_NUM]);
-    MXC_Delay(SEC(2));
+    
+    // block_object(motors[MOTOR_TEST_NUM]);
+    // MXC_Delay(SEC(2));
+    // pull_object(motors[MOTOR_TEST_NUM]);
+    // MXC_Delay(SEC(2));
+    // go_home_forward(motors[MOTOR_TEST_NUM]);
+    // wait_for_home(motors[MOTOR_TEST_NUM]);
+    // MXC_Delay(SEC(2));
+    // block_object(motors[MOTOR_TEST_NUM]);
+    // MXC_Delay(SEC(2));
+    // push_object(motors[MOTOR_TEST_NUM]);
+    // MXC_Delay(SEC(2));
+    // set_target_position(motors[MOTOR_TEST_NUM], motors[MOTOR_TEST_NUM]->maxStep);
     // set_target_position(motors[0], -100);
     printf("Done");
+    MXC_Delay(SEC(1));
+    // set_motor_profile(motors[MOTOR_TEST_NUM], MOTOR_PROFILE_TORQUE);
     // ======================== Main Loop =========================
     while(1) 
-    {
+    {   
+        for(size_t motor_num = 0; motor_num < 3; motor_num++){
+            block_object(motors[motor_num]);
+        }
+        MXC_Delay(SEC(1));
+        for(size_t motor_num = 0; motor_num < 3; motor_num++){
+            pull_object(motors[motor_num]);
+        }
+        MXC_Delay(SEC(3));
+        motor_handler(motors, 3);
+        // go_home_forward(motors[MOTOR_TEST_NUM]);
         
     //     for(int currMotor = FIRST_MOTOR_TEST_NUM; currMotor < LAST_MOTOR_TEST_NUM+1; currMotor++){
     //         // target_tics(currMotor, OUT_TIC_NUM);
@@ -129,6 +152,6 @@ int main()
     //     }
     //     printf("IN\n");
     //     MXC_Delay(SEC(10));
-        MXC_Delay(100);
+        // MXC_Delay(SEC(1));
     }
 }
